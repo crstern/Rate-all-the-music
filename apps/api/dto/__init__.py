4d314a9c1bda71a75d2827eeb7dc32007c28e4cc
Cristian Stern <cristian.stern@red-to-blue.com>
2021-07-30 15:32:28 +0300

    deleted files

--- apps/api/dto/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user_dto import UserDto
-from .album_dto import AlbumDto
-from .image_dto import ImageDto
-from .artist_dto import ArtistDto
--- apps/api/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .album import Album
-from .artist import Artist
-from .rating import Rating
-from .image import Image
-from .comment import Comment
-from .genre import Genre
